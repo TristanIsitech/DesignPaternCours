3c4333d76a7f9c2eea48d819ab59407bddeaeff0
TristanIsitech <tristan.labaume@ecole-isitech.fr>
2023-11-27 14:55:42 +0100

    Ajout de fichier

--- AbstractFactory/obj/Debug/net8.0/AbstractFactory.AssemblyInfo.cs
+++ AbstractFactory/obj/Debug/net8.0/AbstractFactory.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("DesignPaternCours")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("AbstractFactory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cdc5872d0173d8d49b9ab3beddee1691878a93f")]
-[assembly: System.Reflection.AssemblyProductAttribute("DesignPaternCours")]
-[assembly: System.Reflection.AssemblyTitleAttribute("DesignPaternCours")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyProductAttribute("AbstractFactory")]
+[assembly: System.Reflection.AssemblyTitleAttribute("AbstractFactory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
