780dc4707881aa9ce0f08e9174deb594dafa5278
TristanIsitech <tristan.labaume@ecole-isitech.fr>
2023-11-28 14:49:15 +0100

    Composite

---
+++ PatronsStructurels/Composite/Program.cs
@@ -0,0 +1,9 @@
+﻿Société TristanAuto = new Société("TristanAuto", 5);
+TristanAuto.AddSociétéChilds(new Société("TristanAutoFiliale1", 12));
+TristanAuto.AddSociétéChilds(new Société("TristanAutoFiliale2", 4));
+TristanAuto.AddSociétéChilds(new Société("TristanAutoFiliale3", 8));
+TristanAuto.AddSociétéChilds(new Société("TristanAutoFiliale4", 3));
+
+TristanAuto.childs[2].AddSociétéChilds(new Société("UneAutreFiliale", 5));
+
+Console.WriteLine("Le nombre de véhicule total : " + TristanAuto.GetNbVehicules().ToString());
---
+++ PatronsStructurels/Composite/Société.cs
@@ -0,0 +1,20 @@
+public class Société{
+    public string name;
+    public int nbVehicules;
+    public Société(string name, int nbVehicules){
+        this.name = name;
+        this.nbVehicules = nbVehicules;
+    }
+    public List<Société> childs = new List<Société>();
+    public void AddSociétéChilds(Société newChild){
+        childs.Add(newChild);
+    }
+
+    public int GetNbVehicules(){
+        int results = nbVehicules;
+        foreach(Société aChilds in childs){
+            results += aChilds.GetNbVehicules();
+        }
+        return results;
+    }
+}
