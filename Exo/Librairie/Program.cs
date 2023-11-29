d9b78e8b86fa90afd54af32b6fa0264482cf8ccd
TristanIsitech <tristan.labaume@ecole-isitech.fr>
2023-11-29 14:26:16 +0100

    Program.cs et test du fonctionnement des classes

--- Exo/Librairie/Program.cs
+++ Exo/Librairie/Program.cs
@@ -1,3 +1,13 @@
-﻿
-Console.WriteLine("Hello, World!");
-    
+﻿// On creer la bibliotheque :
+BibliothequeDecorator bibliotheque = new BibliothequeDecorator();
+
+// On creer un libraire pour la bibliotheque :
+Libraire leLibraire = new Libraire("Jo", bibliotheque);
+
+leLibraire.bibliothequeDecorator.AjouterAuteur("UnAuteur");
+leLibraire.bibliothequeDecorator.AjouterLivre("Un Nouveau Livre", bibliotheque.auteurs[0], Categories.Fantaisie, 12.34m);
+
+foreach (Livre unLivre in bibliotheque.livres)
+{
+    Console.WriteLine(unLivre.titre);
+}
