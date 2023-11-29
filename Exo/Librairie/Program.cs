baf333cbd2446844b2e03974c2195c0efa968977
TristanIsitech <tristan.labaume@ecole-isitech.fr>
2023-11-29 15:16:45 +0100

    Program.cs pour tester le fonctionnement

--- Exo/Librairie/Program.cs
+++ Exo/Librairie/Program.cs
@@ -8,8 +8,35 @@ Libraire leLibraire = new Libraire("Jo", bibliotheque);
 leLibraire.bibliothequeDecorator.AjouterAuteur("UnAuteur");
 leLibraire.bibliothequeDecorator.AjouterLivre("Un Nouveau Livre", bibliotheque.auteurs[0], Categories.Fantaisie, 12.34m);
 
-// On test si le programme a correctement ajouté notre livre
+// On test si le programme a correctement ajouté notre livre :
+Console.WriteLine("Les livres de la bibliotheque : ");
 foreach (Livre unLivre in bibliotheque.livres)
 {
     Console.WriteLine(unLivre.ToString());
+}
+
+// On ajout maintenant un nouveau client a la bibliotheque :
+leLibraire.bibliothequeDecorator.AjouterSenior("Michelle", 50);
+
+// On stock le nouveau client dans une variable pour l'utiliser plus facilement
+Client? Michelle = bibliotheque.GetClient("Michelle");
+if (Michelle is not null)
+{
+    // Se client vas maintenant emprunter un livre :
+    bibliotheque.EmprunterUnLivre(Michelle, bibliotheque.livres[0]);
+
+    // On test si le livre a bien ete ajoute, et si la somme a correctement ete deduite :
+    Console.WriteLine("\nLes livres de Michelle (" + Michelle.livres.Count() + ") : ");
+    foreach (Livre unLivreDeMichelle in Michelle.livres)
+    {
+        Console.WriteLine(unLivreDeMichelle.ToString());
+    }
+    Console.WriteLine("Son portfeuil : " + Michelle.portfeuil);
+
+    // Puis on test si le livre a bien été supprimé de la bibliotheque :
+    Console.WriteLine("\nLes livres de le bibliotheque (" + bibliotheque.livres.Count() + ") : ");
+    foreach (Livre unLivreDeLeBibliotheque in bibliotheque.livres)
+    {
+        Console.WriteLine(unLivreDeLeBibliotheque.ToString());
+    }
 }
