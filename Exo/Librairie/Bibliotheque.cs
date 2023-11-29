9fcb59bc6394bf4aa2beabdf5e5d1427bbe60fd8
TristanIsitech <tristan.labaume@ecole-isitech.fr>
2023-11-29 12:21:56 +0100

    Update structure Bibliotheque

--- Exo/Librairie/Bibliotheque.cs
+++ Exo/Librairie/Bibliotheque.cs
@@ -1,7 +1,17 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 public class Bibliotheque
 {
     public List<Livre> livres = new List<Livre>();
     public List<Auteur> auteurs = new List<Auteur>();
+
+    public Libraire libraire;
+    public List<Client> clients = new List<Client>();
+
+    public Bibliotheque(Libraire libraire)
+    {
+        this.libraire = libraire;
+    }
     public List<Livre> SuivreAuteur(string nom)
     {
         return new List<Livre>();
@@ -22,6 +32,20 @@ public class BibliothequeDecorator : Bibliotheque
 {
     public LivreBuilder livreBuilder = new LivreBuilder();
     public AuteurBuilder auteurBuilder = new AuteurBuilder();
+
+    public BibliothequeDecorator(Libraire libraire) : base(libraire)
+    {
+        this.libraire = libraire;
+    }
+
+    public void AjouterLivre(string titre, Auteur auteur, Categories categories, decimal prix)
+    {
+        livres.Add(livreBuilder.AjouterLivre(titre, auteur, categories, prix));
+    }
+    public void AjouterAuteur(string nom)
+    {
+        auteurs.Add(auteurBuilder.AjouterAuteur(nom));
+    }
 }
 
 public enum Categories
